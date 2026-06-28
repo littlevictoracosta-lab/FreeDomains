462029657a83ab2b24654a0dbe524998bc533502
littlevictoracosta-lab <littlevictoracosta@gmail.com>
2026-06-27 20:34:55 -0700

    Add login function to handle user authentication

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ async function signup() {
     const data = await res.json();
     document.getElementById("msg").innerText = data.message || data.error;
 }
+
+
+
+
+async function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (data.token) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("username", data.username);
+        alert("Logged in!");
+        window.location.href = "dashboard.html";
+    } else {
+        alert(data.error);
+    }
+}
